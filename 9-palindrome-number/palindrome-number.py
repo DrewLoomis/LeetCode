28ef7f4c5d00e2a08532a0be896dba1d012ae607
DrewLoomis <45793993+DrewLoomis@users.noreply.github.com>
2025-07-16 13:35:51 -0400

    Update palindrome-number.py

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        #convert int to str and then reverse
         reversed = str(x)[::-1]
+        #if we have a palindrome
         if (reversed == str(x)):
             return True
         else:
