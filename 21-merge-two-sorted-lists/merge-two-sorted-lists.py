e4d8ff0ec55f838875cd90a30d82144b4d1ad686
DrewLoomis <45793993+DrewLoomis@users.noreply.github.com>
2025-10-15 09:43:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.70%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,9 +5,12 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
+        #dummy node is used for referencing the head of the linkedList as thats prone to changing
         mergedList = dum = ListNode()
+        #double while for looping through both lists
         while list1 and list2:
+            #since the lists are in increasing order we can check for which one to add 
+            #to the merged list
             if list1.val < list2.val:
                 mergedList.next = list1
                 list1, mergedList = list1.next, list1
@@ -15,6 +18,7 @@ class Solution:
                 mergedList.next = list2
                 list2, mergedList = list2.next, list2
 
+        #checks for empty list
         if list1 or list2:
             mergedList.next = list1 if list1 else list2
         return dum.next
