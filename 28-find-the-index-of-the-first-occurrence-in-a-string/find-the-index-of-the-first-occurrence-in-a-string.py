0839a91f95dbba23f2a782539c0b210dfb3995a7
DrewLoomis <45793993+DrewLoomis@users.noreply.github.com>
2025-10-16 14:58:12 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (81.42%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,16 +3,8 @@ class Solution:
         
         for i in range(len(haystack)):
             if (haystack[i] == needle[0]):
-                #print(i)
                 sub = haystack[i:i + len(needle)]  
-                print(sub)
-                print(needle)
-                #print(len(needle))
-                print(sub == needle)
                 if (sub == needle):
-                    print(True)
                     return i
-                else:
-                    print("false")
         return -1
         
